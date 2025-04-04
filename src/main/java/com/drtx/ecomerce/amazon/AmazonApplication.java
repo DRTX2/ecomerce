bd36d221f9f3927908897dfedc6e863159ffdf2d
DRTX2 <davidmanjarres2004@gmail.com>
2025-04-04 00:03:41 -0500

    Base structure

---
+++ src/main/java/com/drtx/ecomerce/amazon/AmazonApplication.java
@@ -0,0 +1,13 @@
+package com.drtx.ecomerce.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonApplication.class, args);
+	}
+
+}
