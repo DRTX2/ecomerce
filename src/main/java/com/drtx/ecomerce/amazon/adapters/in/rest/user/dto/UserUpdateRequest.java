f92a1e77ce9eb0a4f7e8fd899ca6b8d3a28fbf16
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-03 18:33:45 -0500

    Edit required fields

--- src/main/java/com/drtx/ecomerce/amazon/adapters/in/rest/user/dto/UserUpdateRequest.java
+++ src/main/java/com/drtx/ecomerce/amazon/adapters/in/rest/user/dto/UserUpdateRequest.java
@@ -2,11 +2,12 @@ package com.drtx.ecomerce.amazon.adapters.in.rest.user.dto;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserUpdateRequest(
-        @Nullable String name,
+        @NotBlank String name,
         @Email String email,
-        @Nullable String role,
+        @NotBlank String role,
         @Nullable String address,
         @Nullable String phone
 ) {
