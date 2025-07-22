1779c9c149ab08bfd88422ca3204c85a34a6c2b9
David Manjarres <davidmanjarres2004@gmail.com>
2025-07-21 23:46:42 -0500

    Edit user for auth module

---
+++ src/main/java/com/drtx/ecomerce/amazon/adapters/in/security/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.drtx.ecomerce.amazon.adapters.in.security;
+
+import com.drtx.ecomerce.amazon.adapters.out.persistence.user.UserEntity;
+import com.drtx.ecomerce.amazon.adapters.out.persistence.user.UserPersistenceMapper;
+import com.drtx.ecomerce.amazon.adapters.out.persistence.user.UserPersistenceMapperImpl;
+import com.drtx.ecomerce.amazon.adapters.out.persistence.user.UserPersistenceRepository;
+import com.drtx.ecomerce.amazon.core.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+    private final UserPersistenceRepository repository;
+    private final UserPersistenceMapper mapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        UserEntity userEntity = repository.findByEmail(email)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+        User user =mapper.toDomain(userEntity);
+        return new SecurityUserDetails(user);
+    }
+}
