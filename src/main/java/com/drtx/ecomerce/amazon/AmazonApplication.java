aeeaaf09d5dd1e53898c584590bf4a2365659139
David Manjarres <davidmanjarres2004@gmail.com>
2025-12-22 14:22:35 -0500

    config: add JPA repository and entity scanning configuration
    - Add @EnableJpaRepositories for persistence and security packages
    - Add @EntityScan for persistence entities and security tokens
    - Enable proper scanning of RevokedTokenRepository and RevokedToken entity

--- src/main/java/com/drtx/ecomerce/amazon/AmazonApplication.java
+++ src/main/java/com/drtx/ecomerce/amazon/AmazonApplication.java
@@ -2,7 +2,9 @@ package com.drtx.ecomerce.amazon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -10,6 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 		"com.drtx.ecomerce.amazon.application",
 		"com.drtx.ecomerce.amazon.adapters",
 		"com.drtx.ecomerce.amazon.infrastructure"
+})
+@EnableJpaRepositories(basePackages = {
+		"com.drtx.ecomerce.amazon.adapters.out.persistence",
+		"com.drtx.ecomerce.amazon.adapters.in.security"
+})
+@EntityScan(basePackages = {
+		"com.drtx.ecomerce.amazon.adapters.out.persistence.entity",
+		"com.drtx.ecomerce.amazon.adapters.in.security"
 })
 public class AmazonApplication {
 
