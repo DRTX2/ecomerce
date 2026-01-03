60837f3e2744c770e5baab2e0a2e1697006c9282
David Manjarres <davidmanjarres2004@gmail.com>
2026-01-02 23:00:43 -0500

    feat: update user roles in domain model

--- src/main/java/com/drtx/ecomerce/amazon/core/model/user/UserRole.java
+++ src/main/java/com/drtx/ecomerce/amazon/core/model/user/UserRole.java
@@ -3,13 +3,14 @@ package com.drtx.ecomerce.amazon.core.model.user;
 public enum UserRole {
     USER,
     ADMIN,
+    SELLER,
     MODERATOR;
 
-    public String getAuthority(){
-        return "ROLE_"+this.name();
+    public String getAuthority() {
+        return "ROLE_" + this.name();
     }
 
-    public String roleName(){
+    public String roleName() {
         return this.name();
     }
 }
